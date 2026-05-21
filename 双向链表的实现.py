1ddb21aa8941930a77076715b2e72ef6ac42df97
yuan-521 <3594568055@qq.com>
2026-05-21 16:24:22 +0800

    增加删除

--- 双向链表的实现.py
+++ 双向链表的实现.py
@@ -36,8 +36,6 @@ class DoubleLinkList:
             node.next=self.head     #插入的节点指向原来的头节点
             self.head=node      #把新建的node节点作为头节点
 
-
-
         self._length +=1
 
 
@@ -48,11 +46,14 @@ class DoubleLinkList:
             self.add(new_date)
         else:         #链表不为空，从头节点依次往下找，直到最后节点
             cur=self.head
-            while cur.next !=self.head:
+            while cur.next:
                 cur=cur.next  #cur成为最后一个节点
-            cur.next=node    #尾节点指向插入的真正尾节点
-            node.next=self.head
+
+            node.prev=cur   #新建节点node 的prev 指向原来的尾节点
+            cur.next=node    #原来的尾节点指向新建的node 节点
         self._length += 1
+
+
 
     def insert(self,new_date,pos):  #把新的数据插入到指定链表的位置，
         node = Node(new_date)
@@ -67,35 +68,30 @@ class DoubleLinkList:
             while pos-1:
                 cur=cur.next
                 pos-=1
-            node.next=cur.next    #原来是cur~cur.next.变成cur     node~cur.next
-            cur.next=node
+            node.prev=cur # 新建node 节点的prev指向原来的前一个节点
+            node.next=cur.next    #新建node 节点的next指向原来前一个节点的next 节点
+            cur.next.prev=node  #原来前一个节点的next 节点的prev指向新的node节点
+            cur.next=node    #原来前一个节点的next 指向当前新建的node节点
             self._length+=1
 
 
     def remove(self,date):
         '删除指定的节点的数据'
         cur=self.head
-        pre =None
-        flag=True                     #第一次循环的标记。让循环链表只遍历一次
-        while cur and flag: #如果当前节点存在并且
-            if cur.next==self.head:   #循环遍历至尾节点，第一次循环完成
-                flag=False    #不满足83行，终止循环
-            if cur.date==date:                       #判断删除的数据是不是当前节点的数据
-                if pre:                              #如果有前一个节点
-                    pre.next=cur.next         #删除操作：前一个节点的下一个指向当前的下一个
-                else:                               #要删除的节点就是头节点
-                    #先找到尾节点
-                    last_node=self.head
-                    while last_node.next!=self.head:
-                        last_node.head=last_node.next      #找到尾节点
-                    last_node.next=self.head.next
-                    self.head=cur.next
 
-                self._length-=1
+        while cur : #如果当前节点存在
+
+            if cur.date==date:        #cur:找到了要删除的当前节点
+                if cur==self.head:  #当前要删的节点为头节点head
+                    self.head=cur.next
+                else:
+                    cur.prev.next=cur.next
+                if cur.next:
+                    cur.next.prev=cur.next
+                    self._length-=1
                 return True
             else:
              '没有找到，继续往下便利'
-            pre = cur
             cur = cur.next
         return False
 
