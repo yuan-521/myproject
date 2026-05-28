fb58b51717e1069a3ac6033ea7d5aeab6a5d3f32
yuan-521 <3594568055@qq.com>
2026-05-28 18:19:24 +0800

    对于一个正整数，按逆向顺序输出

---
+++ 逆向输出.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+void main()
+{
+   int a;
+   int b=0;
+   scanf("%d",&a);
+   while(a!=0){
+   
+   
+   
+   b=b*10 +a%10;
+   a/=10;
+   	
+   	
+   	
+   	
+   }
+   printf("%d\n",b);
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
